96afb0bed28e539f38dfa5a935351038a86708b0
Noor Shabib <noorshabib97@gmail.com>
2024-01-18 16:02:14 +0300

    Add assets

---
+++ src/asstes/index.ts
@@ -0,0 +1,21 @@
+export const _assets = {
+    foodTypes : {
+        appetizer: new URL('./FoodTypes/appetizer.png', import.meta.url).href,
+        beverage: new URL('./FoodTypes/beverage.png', import.meta.url).href,
+        bread: new URL('./FoodTypes/bread.png', import.meta.url).href,
+        breakfast: new URL('./FoodTypes/breakfast.png', import.meta.url).href,
+        dessert: new URL('./FoodTypes/dessert.png', import.meta.url).href,
+        drink: new URL('./FoodTypes/drink.png', import.meta.url).href,
+        fingerfood: new URL('./FoodTypes/fingerfood.png', import.meta.url).href,
+        maincourse: new URL('./FoodTypes/maincourse.png', import.meta.url).href,
+        marinade: new URL('./FoodTypes/marinade.png', import.meta.url).href,
+        salad: new URL('./FoodTypes/salad.png', import.meta.url).href,
+        sauce: new URL('./FoodTypes/sauce.png', import.meta.url).href,
+        sidedish: new URL('./FoodTypes/sidedish.png', import.meta.url).href,
+        snack: new URL('./FoodTypes/snack.png', import.meta.url).href,
+        soup: new URL('./FoodTypes/soup.png', import.meta.url).href,
+    },
+    logo:{
+        light:new URL('./Logo/light.png', import.meta.url).href,
+    }
+}
